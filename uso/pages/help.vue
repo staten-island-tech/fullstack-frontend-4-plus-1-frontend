e83147d28e9431d2c26d7fa4323d26b04067b30f
Ehan <ethanliang405@gmail.com>
2022-03-03 12:24:37 -0500

    password

---
+++ uso/pages/help.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="help-page">
+    <div class="under-nav"></div>
+    <div class="help-page-container">
+      <h1>L bozo</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+};
+</script>
+
+<style scoped>
+.help-page-container {
+  margin: auto;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+</style>
