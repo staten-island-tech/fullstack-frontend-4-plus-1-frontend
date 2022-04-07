f4244a338c8b37c47ad8d0e6a201ee29cc352907
kentng01 <57498837+kentng01@users.noreply.github.com>
2022-04-07 12:25:11 -0400

    ddsd

--- uso/components/EndGame.vue
+++ uso/components/EndGame.vue
@@ -30,9 +30,13 @@ export default {
 .endgame__page--container {
   display: flex;
   flex-direction: column;
-  align-items: center;
+  /* align-items: center; */
   height: 100vh;
-  background-image: url('~/assets/images/backgrounds/liyueEndPg.jpg');
+  background-image: linear-gradient(
+      rgba(30, 30, 30, 0.5),
+      rgba(30, 30, 30, 0.5)
+    ),
+    url('~/assets/images/backgrounds/inazumaEndPg.png');
   background-repeat: no-repeat;
   background-size: cover;
   background-attachment: fixed;
