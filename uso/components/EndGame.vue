167bf258a5827bb284ebc68580c436e42343c98a
kentng01 <57498837+kentng01@users.noreply.github.com>
2022-04-11 12:27:20 -0400

    faf

--- uso/components/EndGame.vue
+++ uso/components/EndGame.vue
@@ -5,12 +5,29 @@
   <div>
 
       <div class="endgame__page"> 
-        <!-- <div class="under-nav"></div> -->
-
         <div class="endgame__page--container">
-            <h1>hello world!</h1>
-        </div>
 
+          <div class="endgame__header">
+
+            <div class="header__left">
+              <div id="leftSide">
+                <div id="leftTitle">song // beatmap title</div>
+                <div id="leftTitle"> by... musician // beatmap creator</div>
+                <h3></h3>
+              </div>
+            </div>
+
+            <div class="header__right">
+              <div id="rightSide">Ranking</div>
+            </div>
+
+          </div>
+
+          <div class="endgame__stats">
+            <div id="statsLeft">Content</div>
+          </div>
+
+        </div>
       </div>
     
   </div>
@@ -27,6 +44,27 @@ export default {
 
 <style scoped>
 
+/* declarations */
+
+#leftSide {
+  font-weight: 300;
+}
+
+#leftTitle {
+  font-size: 4rem;
+}
+
+#rightSide {
+  font-size: 8.5rem;
+  font-weight: 500;
+}
+
+#statsLeft {
+  font-size: 6rem;
+}
+
+/* endgame things */
+
 .endgame__page--container {
   display: flex;
   flex-direction: column;
@@ -41,5 +79,33 @@ export default {
   background-size: cover;
   background-attachment: fixed;
 }
+
+.endgame__header {
+  padding: 10px 25px 10px 25px;
+  opacity: 0.9;
+
+  background: #292a2b;
+  color: white;
+
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+}
+
+
+.header__left {
+  padding-top: 5px;
+  margin-right: auto;
+}
+
+.header__right {
+  padding-top: 5px;
+  margin-left: auto;
+}
+
+.endgame__stats {
+  padding: 25px;
+}
+
 
 </style>
