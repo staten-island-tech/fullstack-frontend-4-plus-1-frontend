192d5840fdac4fbd64a2187b3427b48c36ea58a0
Ehan <ethanliang405@gmail.com>
2022-04-29 12:31:26 -0400

    profile

--- uso/pages/profile.vue
+++ uso/pages/profile.vue
@@ -5,26 +5,37 @@
     <div id="profile-body">
       <div class="profile-container">
         <div class="cover-photo"></div>
-        <div class="profile-titlebox">
-          <h1 class="profile-title">♬ rankings</h1>
+
+        <div class="profile-main-body">
+          <div class="profile-titlebox">
+            <h1 class="profile-title">♬ rankings</h1>
+          </div>
         </div>
-        <div class="profile-main-body"></div>
       </div>
     </div>
   </div>
+  <!-- copy osu css layout for pfp and header -->
 </template>
 
 <script>
-export default {};
+import user from '~/static/user.json';
+
+export default {
+  data() {
+    return {
+      user,
+    };
+  },
+  created() {
+    console.log(user);
+  },
+};
 </script>
 
-<style scoped>
-.under-nav {
-  height: 9.5rem;
-}
-
-.profile-body {
-  width: 100vw;
+<style lang="css" scoped>
+#profile-body {
+  --container-width: 55vw;
+  /* width: 100%; */
 }
 
 .profile-container {
@@ -43,32 +54,10 @@ export default {};
   background-size: cover;
   background-attachment: fixed;
 }
-
-.profile-titlebox {
-  width: 55vw;
-
-  background-color: rgb(8, 36, 83);
-}
-
-.profile-title {
-  display: flex;
-  align-items: center;
-  font-size: 3.5rem;
-  font-weight: 400;
-  color: #f6f6f6;
-  height: 5.5rem;
-  margin: 0.5rem 1rem 0.5rem 3.5rem;
-  padding-top: 0.5rem;
-}
-
 .cover-photo {
-  width: 55vw;
+  width: var(--container-width);
   height: 32rem;
   margin-top: 4rem;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  padding: 2.5rem;
   background-image: linear-gradient(
       rgba(62, 54, 104, 0.7),
       rgba(62, 54, 104, 0.7)
@@ -79,6 +68,29 @@ export default {};
   background-position: center 25%;
 }
 
+.profile-main-body {
+  background-image: linear-gradient(rgba(13, 4, 66, 0.7), rgba(13, 4, 66, 0.7)),
+    url('~/assets/images/backgrounds/fleeting-colors.jpg');
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center center;
+}
+/*
+.profile-titlebox {
+  width: 55vw;
+
+  background-color: rgb(8, 36, 83);
+}
+.profile-title {
+  display: flex;
+  align-items: center;
+  font-size: 3.5rem;
+  font-weight: 400;
+  color: #f6f6f6;
+  height: 5.5rem;
+  margin: 0.5rem 1rem 0.5rem 3.5rem;
+  padding-top: 0.5rem;
+}
 .profile-main-body {
   color: #fff;
   font-size: 15rem;
@@ -95,6 +107,6 @@ export default {};
     url('~/assets/images/backgrounds/fleeting-colors.jpg');
   background-repeat: no-repeat;
   background-size: cover;
-  /* background-position: center center; */
-}
+  /* background-position: center center;
+} */
 </style>
