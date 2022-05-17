f65785af7f8155bf570e407887aad9db372caf97
Wilson <71456315+wilsonw13@users.noreply.github.com>
2022-05-17 08:24:22 -0400

    harvey somehow added more node_modules??

--- uso/plugins/inject.js
+++ uso/plugins/inject.js
@@ -1,19 +1,17 @@
-// import createjs from 'createjs/builds/createjs-2015.11.26.min.js'; // https://stackoverflow.com/questions/47540846/dependency-not-found-even-defined-in-package-json-and-node-modules
-
 import Vue from 'vue';
 
 import { Howl, Howler } from 'howler';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faUserSecret } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+// import createjs from 'createjs/builds/createjs-2015.11.26.min.js'; // https://stackoverflow.com/questions/47540846/dependency-not-found-even-defined-in-package-json-and-node-modules
+
+Vue.config.productionTip = false;
+
 library.add(faUserSecret);
 
 Vue.component('font-awesome-icon', FontAwesomeIcon);
 
-Vue.config.productionTip = false;
-// import { createjs } from 'createjs/builds/createjs-2015.11.26.min.js';
-
-// import Howler from 'howler';
 Vue.use(Howl);
 Vue.use(Howler);
-// Vue.use(createjs);
