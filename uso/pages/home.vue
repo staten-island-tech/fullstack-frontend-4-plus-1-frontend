02863d191503115d110706e8295ff0bd78e1171a
Ehan <ethanliang405@gmail.com>
2022-05-19 12:29:16 -0400

    in the middle of updating feature cards in homepage temp rework

--- uso/pages/home.vue
+++ uso/pages/home.vue
@@ -17,11 +17,53 @@
     <section class="home-main">
       <div class="home-content-container">
         <section class="feature-songs home-section">
-          <feature-song-card :v-for="song in featureSongs" />
+          <!-- <feature-song-card :v-for="song in featureSongs" /> -->
+
+          <feature-song-card
+            card-title="Six Trillion Years and Overnight Story"
+            card-artist="kemu feat. IA"
+            card-route="beatmaps"
+            card-img="/beatmaps/1213119/rkc.jpg"
+          />
+          <feature-song-card
+            card-title="Jingle Bells"
+            card-artist="Altria Pendragon (Santa Alter) (CV : Ayako Kawasumi)"
+            card-route="beatmaps"
+            card-img="/beatmaps/689134/BG.jpg"
+          />
+          <feature-song-card
+            card-title="Goodbye Moonmen"
+            card-artist="The Living Tombstone feat. Fart"
+            card-route="beatmaps"
+            card-img="/beatmaps/1249137/mJGRHh3.jpg"
+          />
+          <feature-song-card
+            card-title="Yoru ni Kakeru"
+            card-artist="YOASOBI"
+            card-route="/beatmaps"
+          />
+          <feature-song-card
+            card-title="Unravel"
+            card-artist="Toru Kitajima"
+            card-route="/beatmaps"
+            card-img="https://i.pinimg.com/736x/47/50/49/47504980e845df1adc7677e8fa09e44c--kaneki-wallpaper-tokyo-ghoul.jpg"
+          />
+          <feature-song-card
+            card-title="Brave Shine"
+            card-artist="Aimer"
+            card-route="/beatmaps"
+            card-img="https://images5.alphacoders.com/470/thumb-1920-470240.jpg"
+          />
+          <feature-song-card
+            card-title="Dawn Blue"
+            card-artist="Mori Calliope"
+            card-route="/beatmaps"
+            card-img="https://images.alphacoders.com/111/thumb-1920-1113744.png"
+          />
         </section>
         <section class="news home-section">
           <h1 class="news-header">!USO NEWS</h1>
-          
+
           <news-card />
         </section>
       </div>
@@ -43,11 +85,20 @@ export default {
 
   data() {
     return {
-      featureSongs: [],
+      featureSongs: [
+        {
+          cardType: 'Featured Map',
+          cardTitle: 'Yoru ni Kakeru',
+          cardArtist: 'YOASOBI',
+          cardRoute: '/beatmaps',
+          cardImg:
+            'https://steamuserimages-a.akamaihd.net/ugc/1758061978083459138/795D1B88F10FBF6BA70A54F2477DF95A8D1A36C6/?imw=5000&imh=5000&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false',
+        },
+      ],
     };
   },
   created() {
-    this.getFeatureCards();
+    /* this.getFeatureCards(); */
   },
   methods: {
     /* getFeatureCards: async function getUser() {
@@ -59,15 +110,15 @@ export default {
       }
     }, */
     getFeatureCards: async function getFeatureCards() {
-      try {
-        /* const data = await this.$axios.$get('data.json'); */
+      /*  try {
+        const data = await this.$axios.$get('data.json');
         const data = await fetch('../static/data.json');
         console.log(data + 'fetched data');
         this.featureSongs = data.json;
         console.log(this.featureSongs);
       } catch (error) {
         console.log(error);
-      }
+      } */
     },
   },
 };
