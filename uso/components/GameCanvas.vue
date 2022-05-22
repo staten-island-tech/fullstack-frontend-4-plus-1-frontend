002794e4388a89dbeb452f1b8f0b9ae6b4ce2f70
Wilson <71456315+wilsonw13@users.noreply.github.com>
2022-05-22 16:11:59 -0400

    Revert "test dy change"
    This reverts commit c3ea6c297ae76afd839ce04e25c0f219542f4b7e.

--- uso/components/GameCanvas.vue
+++ uso/components/GameCanvas.vue
@@ -265,7 +265,7 @@ export default {
 
       t.dy =
         (this.scrollSpeed * 1000 * this.stageHeight) /
-        (6860 * this.hitPercent + 6860);
+        (this.stageFPS * (6860 * this.hitPercent + 6860));
       /* t.dy =
         (this.scrollSpeed * 1000 * this.stageHeight) /
         (this.stageFPS * (6860 * this.hitPercent + 6860)); */
@@ -513,7 +513,8 @@ export default {
           this.remainingTime =
             note.time -
             t.beatmapIntro -
-            (1000 * t.stageHeight * t.hitPercent + t.radius) / t.dy;
+            (1000 * t.stageHeight * t.hitPercent + t.radius) /
+              (t.dy * t.stageFPS);
 
           t.notesToFallArray.push(this);
 
@@ -529,8 +530,12 @@ export default {
 
         msFrom(isAbs = false) {
           return isAbs
-            ? Math.abs(((this.y - t.stageHeight * t.hitPercent) * 1000) / t.dy)
-            : ((this.y - t.stageHeight * t.hitPercent) * 1000) / t.dy;
+            ? Math.abs(
+                ((this.y - t.stageHeight * t.hitPercent) * 1000) /
+                  (t.dy * t.stageFPS)
+              )
+            : ((this.y - t.stageHeight * t.hitPercent) * 1000) /
+                (t.dy * t.stageFPS);
         }
 
         miss() {
@@ -631,10 +636,8 @@ export default {
           }
         }
 
-        animateFade(delta) {
-          const da =
-            (t.dy * delta) /
-            (t.stageHeight * (1 - t.hitPercent) + 2 * t.radius);
+        animateFade() {
+          const da = t.dy / (t.stageHeight * (1 - t.hitPercent) + 2 * t.radius);
 
           if (this.alpha - da <= 0) {
             this.alpha = 0;
@@ -670,7 +673,8 @@ export default {
 
           this.i = note.columnIndex;
           this.time = note.time;
-          this.sliderHeight = (t.dy * (note.endTime - note.time)) / 1000;
+          this.sliderHeight =
+            (t.dy * t.stageFPS * (note.endTime - note.time)) / 1000;
 
           this.animateDrop = this.animateDrop.bind(this);
           this.animateShrink = this.animateShrink.bind(this);
@@ -699,7 +703,8 @@ export default {
           this.remainingTime =
             note.time -
             t.beatmapIntro -
-            (1000 * t.stageHeight * t.hitPercent + t.radius) / t.dy;
+            (1000 * t.stageHeight * t.hitPercent + t.radius) /
+              (t.dy * t.stageFPS);
 
           t.notesToFallArray.push(this);
 
@@ -714,9 +719,11 @@ export default {
           if (position === 'bot') {
             return isAbs
               ? Math.abs(
-                  ((this.y - t.stageHeight * t.hitPercent) * 1000) / t.dy
+                  ((this.y - t.stageHeight * t.hitPercent) * 1000) /
+                    (t.dy * t.stageFPS)
                 )
-              : ((this.y - t.stageHeight * t.hitPercent) * 1000) / t.dy;
+              : ((this.y - t.stageHeight * t.hitPercent) * 1000) /
+                  (t.dy * t.stageFPS);
           } else if (position === 'top') {
             return isAbs
               ? Math.abs(
@@ -724,11 +731,11 @@ export default {
                     this.children[0].y -
                     t.stageHeight * t.hitPercent) *
                     1000) /
-                    t.dy
+                    (t.dy * t.stageFPS)
                 )
               : ((this.y + this.children[0].y - t.stageHeight * t.hitPercent) *
                   1000) /
-                  t.dy;
+                  (t.dy * t.stageFPS);
           } else console.log('Invalid position in slider.msFrom()');
         }
 
@@ -822,7 +829,7 @@ export default {
 
           t.ss.sliderColumnContainers[this.i].removeChild(this);
           t.PIXIapp.ticker.remove(this.animateDrop);
-          t.PIXIapp.ticker.remove(() => (this.children[0].y += t.dy * delta));
+          t.PIXIapp.ticker.remove(() => (this.children[0].y += t.dy));
 
           t.readySliders[this.i] = null;
         }
@@ -847,14 +854,14 @@ export default {
           } else if (this.y + this.children[0].y > t.stageHeight) this.remove();
         }
 
-        animateShrink(delta) {
-          this.children[1].height -= t.dy * delta;
-          this.children[0].y += t.dy * delta;
+        animateShrink() {
+          this.children[1].height -= t.dy;
+          this.children[0].y += t.dy;
 
           if (this.children[0].y >= this.children[2].y) {
             t.PIXIapp.ticker.remove(this.animateShrink);
 
-            t.PIXIapp.ticker.add(() => (this.children[0].y += t.dy * delta));
+            t.PIXIapp.ticker.add(() => (this.children[0].y += t.dy));
 
             this.hit();
           }
